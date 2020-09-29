ad29a82a824393d809f43ea4a01d5c4c072a5224
Artem Viliutich <viliutich.artem@gmail.com>
2020-09-29 11:06:25 +0700

    start settings eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ReactComponent as ReactLogo} from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */} 
+        <ReactLogo className="App-logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
